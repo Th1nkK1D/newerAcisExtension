e8068760ac3ad0af5eb3cd19c1937465c217b8b9
Th1nkK1D <witheep@gmail.com>
2017-10-10 19:58:49 +0700

    Update popup block bypassing

--- newerAcis.js
+++ newerAcis.js
@@ -1,1 +1,5 @@
-window.name = 'mainPopupWindow' // It's a magic
+// It's a magic!
+if(window.name != 'mainPopupWindow') {
+    window.name = 'mainPopupWindow'
+    window.location.replace("https://sinfo.kmutt.ac.th/NewAcis/login.jsf");   
+}
